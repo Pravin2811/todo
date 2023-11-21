deb0dca5d11227a123aa32611d1c4d610eaa55ac
Pravin <cpravinkumar007@gmail.com>
2023-11-21 16:23:35 +0530

    bug fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    desc = db.Column(db.String(500), nullable=False)
+    desc = db.Column(db.String(500), nullable=True)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
